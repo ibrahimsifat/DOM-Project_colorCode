8c1ae9526bb849bde8f77476c1133e1bfab0006b
ibrahimsifat <ibsifat24@gmail.com>
2022-03-11 23:51:57 +0300

    added-rgb-color

--- Project_1/app.js
+++ Project_1/app.js
@@ -5,6 +5,8 @@
  * -- Add a button to copy the color code
  * -- Add a toast message when code copied
  * -- User can type their hax code too
+ * -- Show the RGB color too, But do not to edit it
+ * -- user can also copy the RGB color code 
  */
 
 
@@ -13,28 +15,53 @@
 // gloval variable for toest messgae
 let div = null;
 // Selection the element
-const RGBChangeBTN = document.getElementById('RGBChangeBTN');
+const changeColorbtn = document.getElementById('RGBChangeBTN');
 const RootBG = document.getElementById('root')
 const output = document.getElementById('output');
+const output2 = document.getElementById('output2');
 const copyColorBtn = document.getElementById('copyColorBtn')
+const copyColorBtn2 = document.getElementById('copyColorBtn2')
 
-// rgb color ganerator function
-const RGBGenerator = () => {
+
+// generate color code decimal
+function generateDecimal() {
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
-    // Genarate RBG Color
-    // RGBColor = `rgb(${red},${green},${blue})`
+    return {
+        red,
+        green,
+        blue
+    }
+}
 
-    RGBColor = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+
+// generate haxColor
+function haxGenerator({ red, green, blue }) {
+    const getTwoCode = (value) => {
+        const hax = value.toString(16)
+        return hax.length === 1 ? `0${hax}` : hax
+    }
+    RGBColor = `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(blue)}`
+    return RGBColor;
+}
+
+
+// rgb color ganerator function
+const RGBGenerator = ({ red, green, blue }) => {
+    RGBColor = `rbg(${red},${green},${blue})`
     return RGBColor;
 }
 
 // Change RGB BG color function
-RGBChangeBTN.addEventListener('click', function () {
-    const BGColor = RGBGenerator().substring(1)
-    RootBG.style.backgroundColor = `#${BGColor}`
-    output.value = BGColor;
+changeColorbtn.addEventListener('click', function () {
+    const colorDecimal = generateDecimal()
+    const haxColor = haxGenerator(colorDecimal).substring(1)
+    const rgbColor = RGBGenerator(colorDecimal)
+    RootBG.style.backgroundColor = `#${haxColor}`
+    output.value = haxColor;
+    output2.value = rgbColor;
+
     div.remove();
     div = null;
 
@@ -42,25 +69,31 @@ RGBChangeBTN.addEventListener('click', function () {
 })
 
 //copy to clipboard 
-copyColorBtn.addEventListener('click', function () {
-    const outputValue = output.value
-    navigator.clipboard.writeText(`#${output.value}`)
-    if (div !== null) {
-        div.remove();
-        div = null;
-    }
-    if (isColorValid(outputValue)) {
-        const msg = `#${outputValue} ___copied Successfully`
-        const addClass = 'success-toast-msg'
-        generateToast(msg, addClass)
+function copyClipboard(btn, output) {
+    btn.addEventListener('click', function () {
+        const outputValue = output.value
+        navigator.clipboard.writeText(`#${output.value}`)
+        if (div !== null) {
+            div.remove();
+            div = null;
+        }
+        if (isColorValid(outputValue)) {
+            const msg = `#${outputValue} ___copied Successfully`
+            const addClass = 'success-toast-msg'
+            generateToast(msg, addClass)
 
-    } else {
-        const msg = 'You Enter valid color code'
-        const addClass = 'error-toast-msg'
-        generateToast(msg, addClass)
+        } else {
+            const msg = 'You Enter valid color code'
+            const addClass = 'error-toast-msg'
+            generateToast(msg, addClass)
 
-    }
-})
+        }
+    })
+}
+// calling copy btn
+copyClipboard(copyColorBtn, output)
+copyClipboard(copyColorBtn2, output2)
+
 //Generate toast msg
 function generateToast(text, addClass) {
     div = document.createElement('div')
@@ -87,15 +120,16 @@ function generateToast(text, addClass) {
 
 //check color code 
 function isColorValid(colorCode) {
-    if (colorCode <= 5) return false;
-    return /^[0-9A-Fa-f]{3}$/i.test(colorCode) || /^[0-9A-Fa-f]{6}$/i.test(colorCode)
+    return /^[0-9A-Fa-f]{6}$/i.test(colorCode) || /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/
 }
 
 // input color function 
 output.addEventListener('keyup', function (e) {
     const color = e.target.value;
+    console.log(color);
     if (color && isColorValid(color)) {
-        RootBG.style.backgroundColor = color
+        console.log(true);
+        RootBG.style.backgroundColor = `#${color}`
     }
 
 })
